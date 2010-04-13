3f54746dd685244b9a6930498c05890d61416a9c
Martin Schobert <martin@mailbeschleuniger.de>
2010-04-14 00:57:44 +0200

    Copyright notice updated.

--- lib/QuadTree.h
+++ lib/QuadTree.h
@@ -2,7 +2,7 @@
  
    This file is part of the IC reverse engineering tool degate.
  
-   Copyright 2008, 2009 by Martin Schobert
+   Copyright 2008, 2009, 2010 by Martin Schobert
  
    Degate is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
