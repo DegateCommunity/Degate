e9b8da92cb8d6f3081b69f4d2977dc7b6989f811
Martin Schobert <martin@mailbeschleuniger.de>
2010-08-26 01:02:24 +0200

    Typo in a comment fixed.

--- degate_debug.sh
+++ degate_debug.sh
@@ -12,7 +12,7 @@ export DEGATE_HOME=gui;
 # Set the global image tile cache size in MB.
 # Each image tile has a dimension of 1024x1024 pixels. Each pixel is an RGBA value.
 # That is a single image tile has 4MB. You should adjust the cache size, that at
-# least a whole horizonal row of image tiles fit into the cache. E. g. if your
+# least a whole horizotnal row of image tiles fit into the cache. E. g. if your
 # project usually has a horizontal dimension of 10000 pixel, you need around 10
 # tiles for a tile row. That are around 40 MB cache size. 
 # If you have enough memory, you can multiply this value by the number of layers
