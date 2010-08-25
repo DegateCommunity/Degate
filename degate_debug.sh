93eaca4ed49fa4fecbe07630926077a6e30baba5
Martin Schobert <martin@mailbeschleuniger.de>
2010-08-26 00:57:23 +0200

    Use environment variables for the launch of degate.

--- degate_debug.sh
+++ degate_debug.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
+
+# This script will launch degate in a debug mode. That is basically a hack, because
+# we use gdb to generate a stacktrace, if the application crashes.
+
+
+# First we set environment variables.
+
+# This is mandatory, because degate must know where all the glade files and icons are.
 export DEGATE_HOME=gui;
+
+# Set the global image tile cache size in MB.
+# Each image tile has a dimension of 1024x1024 pixels. Each pixel is an RGBA value.
+# That is a single image tile has 4MB. You should adjust the cache size, that at
+# least a whole horizonal row of image tiles fit into the cache. E. g. if your
+# project usually has a horizontal dimension of 10000 pixel, you need around 10
+# tiles for a tile row. That are around 40 MB cache size. 
+# If you have enough memory, you can multiply this value by the number of layers
+# you normally use. 
+export DEGATE_CACHE_SIZE=256
+
+# Set the temp directory.
+export DEGATE_TEMP=/tmp
+
+# Launch degate.
 gdb -x .gdb_commands -q $DEGATE_HOME/degate_bin
 
 
