0743ce5bb61937262d7627697de21fcc77c6db9e
Martin Schobert <martin@schobert.cc>
2018-09-23 19:10:53 +0200

    test projects added

---
+++ test-projects/extract.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cat degate-0.0.6_testproject-mifare.tgz.* > degate-0.0.6_testproject-mifare.tgz
+tar xvzf degate-0.0.6_testproject-mifare.tgz
+
+cat degate-0.1.0_2012-04-22_1639_SC14421CVF_dect.zip.* > degate-0.1.0_2012-04-22_1639_SC14421CVF_dect.zip
+unzip degate-0.1.0_2012-04-22_1639_SC14421CVF_dect.zip
+
+cat degate-0.1.0_2011-05-05_1607_legic_prime.zip.* > degate-0.1.0_2011-05-05_1607_legic_prime.zip
+unzip degate-0.1.0_2011-05-05_1607_legic_prime.zip
