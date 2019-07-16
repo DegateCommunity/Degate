3b31b137e7694b000c945eb74dac3f9787b0f692
DorianBDev <dorian.bachelot@hotmail.com>
2019-07-16 20:56:29 +0200

    Added a test app to the main.cc fille.

--- lib/main.cc
+++ lib/main.cc
@@ -1,10 +1,16 @@
 #include <QApplication>
+#include <QPushButton>
+#include <stdio.h>
 
 
 int main(int argc, char* argv[])
 {
+	printf("Test");
+
 	QApplication a(argc, argv);
 
+	QPushButton bn("Test");
+	bn.show();
 
 	return a.exec();
 }
