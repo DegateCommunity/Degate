b78a01c84e48873d6476c1e21fc6b46f7cbf1918
DorianBDev <dorian.bachelot@hotmail.com>
2019-07-22 11:56:56 +0200

    Deleted the tools folder.

--- tools/gate_lib_documentation/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PRJ_DIR=/home/martin/degate/2011-03-07_2228_legic_prime/subproject_1/
-OUT_DIR=/home/martin/degate
-REL_DIR=../../../degate
-
-./tools/gate_lib_documentation/gate_lib_doc --project-dir $PRJ_DIR --out-dir $OUT_DIR --rel-dir $REL_DIR
