962dba74d2b8459ec3d41625fb3a130c1383ddcd
DorianBDev <dorian.bachelot@hotmail.com>
2020-06-14 12:07:50 +0200

    Added a slider widget class that support double numbers.

---
+++ src/GUI/Widget/DoubleSliderWidget.cc
@@ -0,0 +1,144 @@
+/* -*-c++-*-
+
+  This file is part of the IC reverse engineering tool degate.
+
+  Copyright 2008, 2009, 2010 by Martin Schobert
+
+  Degate is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  Degate is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with degate. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "DoubleSliderWidget.h"
+
+#include <iostream>
+#include <cmath>
+
+namespace degate
+{
+
+    DoubleSliderWidget::DoubleSliderWidget(QWidget *parent)
+            : QWidget(parent)
+    {
+        slider.setOrientation(Qt::Horizontal);
+        slider.setSingleStep(1);
+
+        set_single_step(single_step);
+        set_minimum(minimum);
+        set_maximum(maximum);
+
+        spin_box.setValue(minimum);
+
+        layout.addWidget(&slider);
+        layout.addWidget(&spin_box);
+
+        setLayout(&layout);
+
+        QObject::connect(&slider, SIGNAL(valueChanged(int)),this, SLOT(notify_slider_value_changed(int)));
+        QObject::connect(&spin_box, SIGNAL(valueChanged(double)),this, SLOT(notify_spin_box_value_changed(double)));
+    }
+
+    void DoubleSliderWidget::set_single_step(double single_step)
+    {
+        assert(single_step != 0);
+
+        this->single_step = single_step;
+
+        update_range();
+
+        spin_box.setSingleStep(single_step);
+    }
+
+    double DoubleSliderWidget::get_single_step()
+    {
+        return single_step;
+    }
+
+    void DoubleSliderWidget::set_minimum(double minimum)
+    {
+        this->minimum = minimum;
+        update_range();
+    }
+
+    double DoubleSliderWidget::get_minimum()
+    {
+        return minimum;
+    }
+
+    void DoubleSliderWidget::set_maximum(double maximum)
+    {
+        this->maximum = maximum;
+        update_range();
+    }
+
+    double DoubleSliderWidget::get_maximum()
+    {
+        return maximum;
+    }
+
+    void DoubleSliderWidget::set_value(double value)
+    {
+        this->value = value;
+        slider.setValue((value - minimum) / single_step);
+        spin_box.setValue(value);
+    }
+
+    double DoubleSliderWidget::get_value()
+    {
+        return value;
+    }
+
+    void DoubleSliderWidget::set_decimals(int decimals)
+    {
+        this->decimals = decimals;
+        spin_box.setDecimals(decimals);
+    }
+
+    int DoubleSliderWidget::get_decimals()
+    {
+        return decimals;
+    }
+
+    void DoubleSliderWidget::notify_slider_value_changed(int value)
+    {
+        this->value = value * single_step + minimum ;
+
+        spin_box.blockSignals(true);
+        spin_box.setValue(this->value);
+        spin_box.blockSignals(false);
+
+        emit value_changed(this->value);
+    }
+
+    void DoubleSliderWidget::notify_spin_box_value_changed(double value)
+    {
+        this->value = value;
+
+        slider.blockSignals(true);
+        slider.setValue((value - minimum) / single_step);
+        slider.blockSignals(false);
+
+        emit value_changed(value);
+    }
+
+    void DoubleSliderWidget::update_range()
+    {
+        slider.setMinimum(0);
+        slider.setMaximum(std::ceil((maximum - minimum) / single_step));
+
+        spin_box.setMinimum(minimum);
+        spin_box.setMaximum(maximum);
+
+        set_value(value);
+    }
+}
---
+++ src/GUI/Widget/DoubleSliderWidget.h
@@ -0,0 +1,167 @@
+/* -*-c++-*-
+
+  This file is part of the IC reverse engineering tool degate.
+
+  Copyright 2008, 2009, 2010 by Martin Schobert
+
+  Degate is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  Degate is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with degate. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef __DOUBLESLIDER_H__
+#define __DOUBLESLIDER_H__
+
+#include <QSlider>
+#include <QDoubleSpinBox>
+#include <QHBoxLayout>
+
+namespace degate
+{
+    /**
+     * @class DoubleSliderWidget
+     * @brief Define a double precision slider.
+     * 
+     * @see QSlider
+     */
+    class DoubleSliderWidget : public QWidget
+    {
+    Q_OBJECT
+
+    public:
+        /**
+         * Create a double precision slider.
+         * 
+         * @param parent : the parent of the widget.
+         */
+        explicit DoubleSliderWidget(QWidget* parent = nullptr);
+
+        /**
+         * Set the single step of the slider (the step between values), inferior or equal 1.
+         *
+         * @param single_step : the single step of the slider (inferior or equal 1).
+         */
+        void set_single_step(double single_step);
+
+        /**
+         * Get the single step of the slider (the step between values), inferior or equal 1.
+         *
+         * @return Return the precision value.
+         */
+        double get_single_step();
+
+        /**
+         * Set the minimum possible value for the slider.
+         *
+         * @param minimum : the minimum value.
+         */
+        void set_minimum(double minimum);
+
+        /**
+         * Get the minimum possible value for the slider.
+         *
+         * @return Return the minimum value.
+         */
+        double get_minimum();
+
+        /**
+         * Set the maximum possible value for the slider.
+         *
+         * @param maximum : the maximum value.
+         */
+        void set_maximum(double maximum);
+
+        /**
+         * Get the maximum possible value for the slider.
+         *
+         * @return Return the maximum value.
+         */
+        double get_maximum();
+
+        /**
+         * Set the value, it need to be between the minimum and the maximum values.
+         *
+         * @param value : the new value.
+         */
+        void set_value(double value);
+
+        /**
+         * Get the current value of the slider, it will be between the minimum and the maximum.
+         *
+         * @return Return the current value of the slider.
+         */
+        double get_value();
+
+        /**
+         * Set the precision of the spin box (the number of decimals shown).
+         * The default value will be calculated using the single_step value (but will not be optimal in certain
+         * conditions)
+         *
+         * @param decimals : the number of decimals shown on the spin box.
+         */
+        void set_decimals(int decimals);
+
+        /**
+         * Get the precision of the spin box (the number of decimals shown).
+         * The default value will be calculated using the single_step value (but will not be optimal in certain
+         * conditions). This default value is different from the value returned here and is used when this value is -1.
+         *
+         * @return Return the number of decimals shown on the spin box.
+         */
+        int get_decimals();
+
+    signals:
+        /**
+         * Emitted when the value changed.
+         *
+         * @param value : the new value.
+         */
+        void value_changed(double value);
+
+    public slots:
+        /**
+         * Called when the QSlider value changed.
+         *
+         * @param value : the new value.
+         */
+        void notify_slider_value_changed(int value);
+
+        /**
+         * Called when the QDoubleSpinBox value changed.
+         *
+         * @param value : the new value.
+         */
+        void notify_spin_box_value_changed(double value);
+
+    protected:
+        /**
+         * Update the range of the slider and the spin box regarding min, max and step.
+         */
+        void update_range();
+
+    private:
+        QHBoxLayout    layout;
+        QSlider        slider;
+        QDoubleSpinBox spin_box;
+
+        double value;
+
+        double single_step = 0.1;
+        double minimum     = 0;
+        double maximum     = 1;
+        int    decimals    = -1;
+
+    };
+}
+
+#endif //__DOUBLESLIDER_H__
