3795aea6a2b59ae3533bc2d08b41637b1c9bc396
DorianBDev <dev@dorianb.net>
2021-03-20 10:48:06 +0100

    Rollback the wire matching system to the old 2013 version.

--- src/Core/Matching/WireMatching.cc
+++ src/Core/Matching/WireMatching.cc
@@ -84,27 +84,32 @@ void WireMatching::set_min_edge_magnitude(double min_edge_magnitude)
 
 void WireMatching::run()
 {
-    BinaryLineDetection test(bounding_box.get_min_x(),
-                             bounding_box.get_max_x(),
-                             bounding_box.get_min_y(),
-                             bounding_box.get_max_y(),
-                             wire_diameter,
-                             median_filter_width,
-                             sigma > 0 ? 10 : 0,
-                             sigma);
-    TileImage_GS_DOUBLE_shptr i = test.run(img, TileImage_GS_DOUBLE_shptr(), "/tmp");
-    assert(i != nullptr);
-    //save_normalized_image<TileImage_GS_DOUBLE>(join_pathes(test.get_directory(), "line.tif"), i);
+    auto directory = create_temp_directory();
 
-    LineSegmentExtraction<TileImage_GS_DOUBLE> extraction(i, wire_diameter / 2, 2, test.get_border());
+    ZeroCrossingEdgeDetection ed(bounding_box.get_min_x(),
+                                 bounding_box.get_max_x(),
+                                 bounding_box.get_min_y(),
+                                 bounding_box.get_max_y(),
+                                 median_filter_width,
+                                 sigma > 0 ? 10 : 0,
+                                 sigma,
+                                 wire_diameter >> 1,
+                                 wire_diameter + (wire_diameter >> 1),
+                                 min_edge_magnitude, 0.5);
+
+    TileImage_GS_DOUBLE_shptr i = ed.run(img, TileImage_GS_DOUBLE_shptr(), directory);
+    assert(i != nullptr);
+
+    LineSegmentExtraction<TileImage_GS_DOUBLE> extraction(i, wire_diameter / 2, 2, ed.get_border());
     LineSegmentMap_shptr line_segments = extraction.run();
     assert(line_segments != nullptr);
 
     assert(lmodel != nullptr);
     assert(layer != nullptr);
 
-    BOOST_FOREACH(LineSegment_shptr ls, *line_segments)
+    BOOST_FOREACH (LineSegment_shptr ls, *line_segments)
     {
+        debug(TM, "found wire");
         Wire_shptr w(new Wire(bounding_box.get_min_x() + ls->get_from_x(),
                               bounding_box.get_min_y() + ls->get_from_y(),
                               bounding_box.get_min_x() + ls->get_to_x(),
@@ -113,4 +118,6 @@ void WireMatching::run()
 
         lmodel->add_object(layer->get_layer_pos(), w);
     }
+
+    remove_directory(directory);
 }
